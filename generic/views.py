6b2f721c79cd22ff801352525c3be7dbd8c5a28f
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-05 04:24:57 +0500

    added twitter timeline and resume url

--- generic/views.py
+++ generic/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import django
 from django.conf import settings
@@ -8,6 +8,14 @@ def index(request):
     homepage = HomePage.objects.all()[0]
     context = {'homepage': homepage}
     return render(request, 'index.html', context=context)
+
+def resume(request):
+    resume_url = HomePage.objects.all()[0]
+    if resume_url.resume:
+        resume_url = resume_url.resume.url
+    else:
+        resume_url = 'generic.views.index'
+    return redirect(resume_url)
 
 def about_us(request):
     homepage = AboutUs.objects.all()[0]
