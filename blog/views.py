a01be05764e3d3ae5f8fa9b3aac2caff5d3c6684
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-05 02:16:26 +0500

    blog listing working

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import django
 from django.conf import settings
-
+from .models import Post
 
 def index(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context = {'latest_question_list': latest_question_list}
-    return render(request, 'blog.html')
+    posts = Post.objects.order_by('-published_date')[:5]
+    context = {'posts': posts}
+    return render(request, 'blog.html', context)
