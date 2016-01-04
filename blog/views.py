d8758b33e99e924e1bebdc42986159938c505445
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-05 02:39:22 +0500

    individual post now working

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 import django
 from django.conf import settings
@@ -7,4 +7,9 @@ from .models import Post
 def index(request):
     posts = Post.objects.order_by('-published_date')[:5]
     context = {'posts': posts}
-    return render(request, 'blog.html', context)
+    return render(request, 'blog/index.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/post.html', context)
