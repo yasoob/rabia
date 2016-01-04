d2307c44fa5cbbec04e6c0af190dbc6215a89e00
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-05 02:43:28 +0500

    comments display now working

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Post, PostAdmin)
-#admin.site.register(HomePageImage)
+admin.site.register(Comment)
+
 
