c631779931246ac884834ccfedfdfb63e63ea1c0
Yasoob Khalid <yasoob.khld@gmail.com>
2016-08-16 00:34:36 +0500

    revamped the gallery

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $('#mygallery').justifiedGallery({
   lastRow : 'justify', 
-  rowHeight : 220, 
+  rowHeight : 280, 
   rel : 'gallery2',
-  margins : 3
+  margins : 10
 }).on('jg.complete', function () {
     $('#mygallery a').swipebox();
+});
+
+$('#mygallery img').hover(function(){
+	$('#mygallery img').css("opacity",".3");
+	$(this).css("opacity","1");
+}, function(){
+	$('#mygallery img').css("opacity","1");
 });
